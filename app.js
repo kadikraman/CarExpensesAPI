e11a7ba5a8a9cf613239dd3031513f4446896fcd
Kadi Kraman <hellokadi@gmail.com>
2015-05-02 13:55:47 +0100

    Added a new way of routing in express (both are valid, this one is clearer for more complex APIs)

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ var app = express();
 // gets port number from environment. Defaults to 3000.
 var port = process.env.PORT || 3000;
 
+var expensesRouter = express.Router();
+
+expensesRouter.route('/Expenses')
+    .get(function(req, res){
+        var responseJson = {hello: "This is my api"};
+
+        res.json(responseJson);
+    });
+
+
+app.use('/api', expensesRouter);
+
+
 app.get('/', function(req, res){
    res.send('Welcome to my API!');
 });
