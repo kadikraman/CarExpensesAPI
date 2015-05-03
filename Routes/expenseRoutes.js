820dae55d04e28512f6427865e177c184191c61e
Kadi Kraman <hellokadi@gmail.com>
2015-05-03 15:54:44 +0100

    Added middleware to get the expense from the DB before route call; implemented put, patch and delete on expense

--- Routes/expenseRoutes.js
+++ Routes/expenseRoutes.js
@@ -37,16 +37,76 @@ var routes = function(Expense){
             });
         });
 
+    /**
+     * Middleware. Gets executed just before the request is passed onto the router.
+     * Used as a tool to minimize code duplication, e.g. since all of the requests require getting
+     * the expense from the BD by id, instead of writing the same findById() code many times, write
+     * it once in this middleware function and then pass it in the router.
+     * Note: you can chain middleware.
+     */
+    expenseRouter.use('/:expenseId', function(req, res, next){
+        Expense.findById(req.params.expenseId, function(err, expense){
+            if (err){
+                res.status(500).send(err);
+            }
+            else if (expense){
+                req.expense = expense;
+                next();
+            }
+            else{
+                // if the book is not found, send a 404 (and the request will never go downstream)
+                res.status(404).send('No expense found');
+            }
+        })
+    });
+
     expenseRouter.route('/:expenseId')
         .get(function(req, res){
-            Expense.findById(req.params.expenseId, function(err, expense){
-                if (err){
+            res.json(req.expense);
+        })
+        .put(function(req, res){
+            req.expense.type = req.body.type;
+            req.expense.cost = req.body.cost;
+            req.expense.mileage = req.body.mileage;
+            req.expense.comment = req.body.comment;
+            req.expense.litres = req.body.litres;
+            req.expense.date = req.body.date;
+            req.expense.save(function(err){
+                if(err){
                     res.status(500).send(err);
                 }
                 else{
-                    res.json(expense);
+                    res.json(req.expense);
                 }
-            })
+            });
+        })
+        .patch(function(req, res){
+            if(req.body._id){
+                // we do not want to allow the user to update the id
+                delete req.body._id;
+            }
+            for(var p in req.body){
+                req.expense[p] = req.body[p];
+            }
+            req.expense.save(function(err){
+                if(err){
+                    res.status(500).send(err);
+                }
+                else{
+                    res.json(req.expense);
+                }
+            });
+
+        })
+        .delete(function(req, res){
+            req.expense.remove(function(err){
+                if(err){
+                    res.status(500).send(err);
+                }
+                else{
+                    res.status(204).send('Removed');
+                }
+            });
         });
     return expenseRouter;
 };
