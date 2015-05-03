d8677eae279aff6cf62023522ae6e71e60508632
Kadi Kraman <hellokadi@gmail.com>
2015-05-03 13:30:51 +0100

    Added a connection to MongoDB and a schema

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose');
 
 
+var db = mongoose.connect('mongodb://localhost/expensesAPI');
+
+var Expense = require('./models/expenseModel');
 var app = express();
 
 // gets port number from environment. Defaults to 3000.
@@ -10,9 +14,34 @@ var expensesRouter = express.Router();
 
 expensesRouter.route('/Expenses')
     .get(function(req, res){
-        var responseJson = {hello: "This is my api"};
+        // note: you can just do var query = req.query and pass that in (and it will work), but will
+        // send any user input to the database as long as it's in the params
+        // This is a cleaner way of passing in the paramaters
+        var query = {};
+        if(req.query.type){
+            query.type = req.query.type;
+        }
+        // finds the set of expenses matching the query and sends them back as JSON
+        Expense.find(query, function(err, expenses){
+           if(err){
+               res.status(500).send(err);
+           }
+            else{
+               res.json(expenses);
+           }
+        });
+    });
 
-        res.json(responseJson);
+expensesRouter.route('/Expenses/:expenseId')
+    .get(function(req, res){
+       Expense.findById(req.params.expenseId, function(err, expense){
+           if (err){
+               res.status(500).send(err);
+           }
+           else{
+               res.json(expense);
+           }
+       })
     });
 
 
---
+++ models/expenseModel.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var expenseModel = new Schema({
+    type: {
+        type: String
+    },
+    cost: {
+        type: Number
+    },
+    mileage: {
+        type: Number
+    },
+    date: {
+        type: String
+    },
+    comment: {
+        type: String
+    },
+    litres: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.model('Expense', expenseModel);
