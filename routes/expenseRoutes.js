ff7a1752d998cbbd049901c5275c919920e1c4be
Kadi Kraman <kadi.kraman@red-badger.com>
2015-08-04 11:44:28 +0100

    Added CORS header to expenses

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -34,6 +34,15 @@ var routes = function(Expense, config){
         }
     });
 
+    /**
+    * Adds the CORS header
+    */
+    expenseRouter.use('/', use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     expenseRouter.route('/')
         .post(expenseController.post)
         .get(expenseController.getAll);
