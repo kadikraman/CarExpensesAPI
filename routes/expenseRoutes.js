1a48eccc870a169a36958c58dd2a70b374cf033f
Kadi Kraman <kadi.kraman@red-badger.com>
2015-08-04 12:31:48 +0100

    Too much bracketing.

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -37,11 +37,11 @@ var routes = function(Expense, config){
     /**
     * Adds the CORS header
     */
-    expenseRouter.use('/', use(function(req, res, next) {
+    expenseRouter.use('/', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-    }));
+    });
 
     expenseRouter.route('/')
         .post(expenseController.post)
